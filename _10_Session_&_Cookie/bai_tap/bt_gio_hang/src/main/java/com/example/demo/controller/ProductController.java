1ff3470161ddb0a75bf1a84bb85679c5978a74e9
tinh <thuongtinh2000@gmail.com>
2021-11-10 14:37:44 +0700

    commit

---
+++ _10_Session_&_Cookie/bai_tap/bt_gio_hang/src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,55 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.model.Cart;
+import com.example.demo.model.Product;
+import com.example.demo.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Optional;
+@Controller
+@SessionAttributes("cart")
+public class ProductController {
+    @Autowired
+    private IProductService productService;
+
+    @ModelAttribute("cart")
+    public Cart initCart(){
+        return new Cart();
+    }
+
+    @GetMapping("/shop")
+    public ModelAndView showShop(){
+        ModelAndView modelAndView = new ModelAndView("shop");
+        modelAndView.addObject("productList",productService.findAll());
+        return modelAndView;
+    }
+
+    @GetMapping("/add/{id}")
+    public ModelAndView addProductToCart(@PathVariable(name = "id")Long idProduct, @SessionAttribute("cart") Cart cart, RedirectAttributes redirectAttributes){
+        Optional<Product> product = productService.findById(idProduct);
+        if (!product.isPresent()){
+            return new ModelAndView("error404");
+        }
+        cart.addProduct(product.get());
+        redirectAttributes.addFlashAttribute("msg", product.get().getName() +" đã thêm vào giỏ hàng ~~");
+        return new ModelAndView("redirect:/shop");
+    }
+
+    @GetMapping("/detail/{id}")
+    public ModelAndView showDetail(@PathVariable(name = "id")Long idProduct){
+        Optional<Product> product = productService.findById(idProduct);
+        if (product.isPresent()){
+            ModelAndView modelAndView = new ModelAndView("detail");
+            modelAndView.addObject("productObject", product.get());
+            return modelAndView;
+        }
+        return new ModelAndView("error404");
+    }
+
+
+}
---
+++ _10_Session_&_Cookie/bai_tap/bt_gio_hang/src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+
+import com.example.demo.model.Product;
+import com.example.demo.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService implements IProductService {
+
+
+    @Autowired
+    private IProductRepository productRepository;
+
+
+    @Override
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+}
